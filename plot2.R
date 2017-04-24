0b11b817066ef11bb8ac33cf9aa33645b9784c42
fouadsel88 <fou_inps88@yahoo.fr>
2017-04-24 23:27:55 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -18,6 +18,6 @@ png(filename = "plot2.png", width = 480 ,height = 480  )
 
 
 #plot Globale active power and time.
-plot(Dsub$Time,Dsub$Global_active_power, type = "l" , xlab = "")
- 
+plot(Dsub$Time,Dsub$Global_active_power, type = "l" , xlab = "", ylab = "Global active power (Kilowatts)")
+  
 dev.off()
